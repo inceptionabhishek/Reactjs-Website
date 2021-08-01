8503b041745c66e3d56f71e2d4f62c9a1b942ecc
Abhishek kumar <abhishek1122338@gmail.com>
2021-08-02 01:30:49 +0530

    Uploading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from 'react'
-import RandomData from './RandomData'
+import React, { useState } from "react";
 function App() {
+  const [query, setQuery] = useState("");
+  const [Value, setValue] = useState({});
+
+
+
+ const fixedUrlGender = "https://api.genderize.io?name=";
+ const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${fixedUrlGender}${query}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setValue(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
+  function setImage(props) {
+    if(props=="male"){
+      return (
+        <img src="https://img.icons8.com/color/48/000000/user-male-circle--v2.png"  className="IconImage"/>
+      );
+    }
+
+    return (
+      <img src="https://img.icons8.com/color/48/000000/female-facepalm.png"  className="IconImage"/>
+    );
+    
+  }
   return (
-    <div className="App">
-      <h1 class="Heading-1">FIND YOUR RANDOM FRIENDS</h1>
-      <p className="Para-intro-1">Made Using react just to implement API :P</p>
+    <div className="NewProject">
+      <div className="Intro">
+        <h1>I will predict Your Gender, Try me! </h1>
+      </div>
+      <input
+        type="text"
+        className="search-box"
+        placeholder="search.."
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+        onKeyPress={search}
+      />
+      <div className="Content">
+        <h2>You are </h2>
+        <h1 className="JsonValue">{Value.gender}</h1>
+        <div className="IconImage">{setImage(Value.gender)}</div>
+      </div>
     </div>
   );
 }
